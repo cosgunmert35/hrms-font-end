1066739afff2f23409fc99a84a85324127475e39
Mert <cosgunmert35@gmail.com>
2021-06-21 15:20:06 +0300

    Littile css change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import "semantic-ui-css/semantic.min.css";
-
+import { Container } from "semantic-ui-react";
 import Dashboard from "./layouts/Dashboard";
+import Navi from "./layouts/Navi";
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      <Navi/>
+      <Container className="main">
+        <Dashboard/>
+      </Container>
     </div>
   );
 }
